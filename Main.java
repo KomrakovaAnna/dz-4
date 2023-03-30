95c4d4bc8803ae383b5a517ae872df80c1caab51
KomrakovaAnna <komrakova.anna@gmail.com>
2023-03-30 07:59:08 +0100

    line alignment

--- Main.java
+++ Main.java
@@ -1,11 +1,9 @@
 public class Main {
 
     public static void main(String[] args){
-
         intPrint intP  = new intPrint();
         intP.print();
         int b = intP.a;
         System.out.println("~~~<Returned value " + b + ">~~~");
-
     }
 }
--- intPrint.java
+++ intPrint.java
@@ -1,9 +1,7 @@
 public class intPrint {
-int a = 1;
-
-
+    int a = 1;
     public int print() {
-        System.out.println("---< field value is" +" "+ a + ">---");
+        System.out.println("---< Field value is" +" "+ a + ">---");
         return a;
     }
 }
